66c344a63033eb964d23e5a3db08100e052f592d
221910308052 <221910308052@gitam.in>
2025-10-28 21:42:15 -0700

    Remove old project files, keep only WhichCard1

--- dist/which-card/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XSUDLA4M.mjs";import"./chunk-TLUYTOXK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-RGKPDT3S.mjs";import"./chunk-4BJRDMCL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
